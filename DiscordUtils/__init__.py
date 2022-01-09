83337e361f235c7bb7f92f59e9ea65fd18c73d5a
Itz_aBoy <computer.tech.yt@gmail.com>
2022-01-09 19:16:35 +0500

    Add files via upload

---
+++ DiscordUtils/__init__.py
@@ -0,0 +1,8 @@
+from DiscordUtils import Pagination
+from DiscordUtils.InviteTracker import InviteTracker
+from DiscordUtils.Music import Music
+
+__title__ = "DiscordUtils"
+__version__ = "1.3.4"
+__author__ = "toxicrecker"
+__license__ = "MIT"
